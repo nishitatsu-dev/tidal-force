982f90e2a9814ebd4edea93aa35bc9b4386182eb
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-05-25 18:46:17 +0900

    共用のメソッドをapplication_controllerに移動し、少し修正した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,31 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   private
+  def permitted_params
+    params.permit(:timezone, :first_date, :page)
+  end
+
   def set_timezone_and_first_date
-    session[:timezone] = params[:timezone] || session[:timezone] || "Asia/Tokyo"
-    session[:first_date] = params[:first_date] || session[:first_date] || Date.today.strftime("%Y-%m-%d")
+    session[:timezone] = permitted_params[:timezone] || session[:timezone] || "Asia/Tokyo"
+    session[:first_date] = permitted_params[:first_date] || session[:first_date] || Date.today.strftime("%Y-%m-%d")
+    session[:page] = permitted_params[:page].to_i || session[:page] || 0
+  end
+
+  def get_page
+    permitted_params[:page].to_i || 0
+  end
+
+  def get_db_records(date)
+    date_hour_0000 = Time.zone.parse(date + " 00:00")
+    date_hour_2300 = Time.zone.parse(date + " 23:00")
+    Record.where(recorded_at: (date_hour_0000..date_hour_2300))
+  end
+
+  def make_hourly_records(date, db_records)
+    time_indexed_records = db_records.index_by { |record| record.recorded_at.hour }
+    date_hours = (0..23).map { |hour| Time.zone.parse(date + " " + format("%02d:00", hour)) }
+    (0..23).map do |hour|
+      time_indexed_records[hour] || Record.new(recorded_at: date_hours[hour])
+    end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,31 +3,8 @@ class HomeController < ApplicationController
     set_timezone_and_first_date
     Time.use_zone(session[:timezone]) do
       date = Date.parse(session[:first_date]).advance(days: get_page).strftime("%Y-%m-%d")
-      time_indexed_records = get_time_indexed_records(date)
-      @records = make_hourly_records(date, time_indexed_records)
+      db_records = get_db_records(date)
+      @records = make_hourly_records(date, db_records)
     end
-  end
-
-  private
-  def permitted_params
-    params.permit(:page)
-  end
-
-  def get_page
-    permitted_params[:page].to_i || 0
-  end
-
-  def get_time_indexed_records(date)
-      date_hour_0000 = Time.zone.parse(date + " 00:00")
-      date_hour_2300 = Time.zone.parse(date + " 23:00")
-      records = Record.where(recorded_at: (date_hour_0000..date_hour_2300))
-      records.index_by { |record| record.recorded_at.hour }
-  end
-
-  def make_hourly_records(date, time_indexed_records)
-      date_hours = (0..23).map { |hour| Time.zone.parse(date + " " + format("%02d:00", hour)) }
-      (0..23).map do |hour|
-        time_indexed_records[hour] || Record.new(recorded_at: date_hours[hour])
-      end
   end
 end
