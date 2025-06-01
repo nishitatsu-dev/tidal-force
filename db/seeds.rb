b52fc9b3f3257a5dfbb2efbda28721b29b63f21a
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-06-01 16:02:28 +0900

    seedデータ（development）を作った

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Rails.env.development?
+
+  # User.create!(
+  #   email: "test@test.com",
+  #   password: "testtest"
+  # )
+
+  Record.create!(
+    [
+      { recorded_at: Time.zone.parse("#{Date.today.strftime("%Y-%m-%d")} 06:00 +0900"), column_0: 1.0, column_1: 2.0, column_2: 3.0, column_3: 4.0, column_4: 5.0, column_5: 6.0, memo: "Sample memo", user_id: 1 },
+      { recorded_at: Time.zone.parse("#{Date.today.strftime("%Y-%m-%d")} 07:00 +0900"), column_0: 1.0, column_1: 2.0, column_2: 3.0, column_3: 4.0, column_4: 5.0, column_5: 6.0, memo: "Sample memo", user_id: 1 },
+      { recorded_at: Time.zone.parse("#{Date.today.strftime("%Y-%m-%d")} 08:00 +0900"), column_0: 1.0, column_1: 2.0, column_2: 3.0, column_3: 4.0, column_4: 5.0, column_5: 6.0, memo: "Sample memo", user_id: 1 },
+      { recorded_at: Time.zone.parse("#{Date.today.strftime("%Y-%m-%d")} 16:00 +0900"), column_0: 1.0, column_1: 2.0, column_2: 3.0, column_3: 4.0, column_4: 5.0, column_5: 6.0, memo: "Sample memo", user_id: 1 },
+      { recorded_at: Time.zone.parse("#{Date.today.strftime("%Y-%m-%d")} 17:00 +0900"), column_0: 1.0, column_1: 2.0, column_2: 3.0, column_3: 4.0, column_4: 5.0, column_5: 6.0, memo: "Sample memo", user_id: 1 },
+      { recorded_at: Time.zone.parse("#{Date.today.strftime("%Y-%m-%d")} 18:00 +0900"), column_0: 1.0, column_1: 2.0, column_2: 3.0, column_3: 4.0, column_4: 5.0, column_5: 6.0, memo: "Sample memo", user_id: 1 }
+    ]
+  )
+end
