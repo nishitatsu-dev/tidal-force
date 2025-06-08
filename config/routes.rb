1f16c61abcb6992702f8ff94ae2f4b4b92b2e2f7
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-06-09 06:36:16 +0900

    letter_opener_webの設定を行なった

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # letter_opener_webの設定
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
