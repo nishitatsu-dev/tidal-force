30abdeab69c9c3ee50c83ad2e39d5cca68944f2a
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-06-10 07:41:57 +0900

    timezoneの値のセットを、stimulusを使うように変更した（画面更新に細かく対応する為）

--- app/javascript/timezone.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const first_form = document.forms[0];
-  first_form.timezone.value = Intl.DateTimeFormat().resolvedOptions().timeZone;
-});
