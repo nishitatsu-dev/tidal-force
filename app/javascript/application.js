beec8be78c7d6f8bd3093e262af57b13de12ba35
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-06-24 19:31:41 +0900

    不要な記述を削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "tidal_force_plus"
-import "paginator"
-import "calc_results_page"
-import "records_page"
