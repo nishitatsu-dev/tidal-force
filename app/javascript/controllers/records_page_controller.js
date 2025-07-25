71cb3d37374bbd476af81a17c0408b801d62e206
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-07-25 15:25:02 +0900

    拡張すると思ったがそうならず、読みにくかったので合体した

--- app/javascript/controllers/records_page_controller.js
+++ app/javascript/controllers/records_page_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-import { RecordsPage } from "records_page";
 
 // Connects to data-controller="records-page"
 export default class extends Controller {
@@ -12,10 +11,7 @@ export default class extends Controller {
   showPage(event) {
     const pageId = event ? Number(event.currentTarget.id) : 0;
 
-    const recordsPage = new RecordsPage(
-      this.pageFieldTarget,
-      this.pageFormTarget,
-    );
-    recordsPage.getRecords(pageId);
+    this.pageFieldTarget.value = pageId;
+    this.pageFormTarget.requestSubmit();
   }
 }
