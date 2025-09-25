161b7e92afab129a232be83c70ec4d8571bdc750
nishitatsu-dev <nishitatsu.dev@gmail.com>
2025-09-26 07:32:46 +0900

    表示地点を増やした（一部入れ替え）。合わせて初期表示を変更した

--- app/javascript/tidal_force_plus/index.js
+++ app/javascript/tidal_force_plus/index.js
@@ -14,13 +14,23 @@ export const EARTH_RADIUS = 6378136.6; // m
 export const AU = 149597870700; // m
 
 export const LOCATION = {
-  NEMURO: { longitude: 145.583, latitude: 43.333 },
-  AKITA: { longitude: 140.117, latitude: 39.717 },
-  TOKYO: { longitude: 139.733, latitude: 35.65 },
-  OGASAWARA: { longitude: 142.183, latitude: 27.083 },
-  TAKAMATSU: { longitude: 134.05, latitude: 34.35 },
-  KAGOSHIMA: { longitude: 130.55, latitude: 31.5 },
-  NAHA: { longitude: 127.667, latitude: 26.216 },
+  WAKKANAI: { longitude: 141.683, latitude: 45.4 },
+  NEMURO: { longitude: 145.583, latitude: 43.35 },
+  OTARU: { longitude: 141, latitude: 43.2 },
+  AKITA: { longitude: 140.067, latitude: 39.75 },
+  SOMA: { longitude: 140.967, latitude: 37.833 },
+  TOKYO: { longitude: 139.767, latitude: 35.65 },
+  TOYAMA: { longitude: 137.217, latitude: 36.767 },
+  NAGOYA: { longitude: 136.883, latitude: 35.083 },
+  AKASHI: { longitude: 134.983, latitude: 34.65 },
+  SAKAIMINATO: { longitude: 133.25, latitude: 35.55 },
+  TOSASHIMIZU: { longitude: 132.967, latitude: 32.783 },
+  HAKATA: { longitude: 130.4, latitude: 33.617 },
+  TANEGASHIMA: { longitude: 130.967, latitude: 30.467 },
+  NAHA: { longitude: 127.667, latitude: 26.217 },
+  YONAGUNIJIMA: { longitude: 122.95, latitude: 24.45 },
+  CHICHIJIMA: { longitude: 142.2, latitude: 27.1 },
+  MINAMITORISHIMA: { longitude: 153.983, latitude: 24.283 },
 };
 
 // 起潮力・惑星距離計算の最表層
